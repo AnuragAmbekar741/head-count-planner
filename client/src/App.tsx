6c9a5c201a531b0e5be5889682254b37199c83f5
anurag-branchout <anurag.ambekar@branchout.app>
2025-11-19 01:01:05 -0500

    FE - setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="text-xl text-center">
+      <Button variant={"default"}>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
